0611da118a09757358ddf81ac076b766db5f4ddb
Melissa <melissa.freiser@gmail.com>
2021-07-03 10:17:41 +0200

    Breaks up reducer functions so that it is easier to read

--- src/reducers.js
+++ src/reducers.js
@@ -9,52 +9,68 @@ const getProductIndex = (products, productId) => {
   });
 };
 
+const addProductToObject = (state, action) => {
+  return {
+    products: [...state.products, action.product],
+    total: state.total + action.product.price,
+  };
+};
+
+const removesProductFromObject = (state, productIndex) => {
+  let splicedArray = [...state.products];
+  splicedArray.splice(productIndex, 1);
+  return {
+    products: splicedArray,
+    total: state.total - state.products[productIndex].price,
+  };
+};
+
+const addQuantityToProductInObject = (state, action, productIndex) => {
+  let newState = { ...state };
+  const updatedProduct = {
+    ...state.products[productIndex],
+    quantity: state.products[productIndex].quantity + 1,
+  };
+  let splicedArray = [...newState.products];
+  splicedArray.splice(productIndex, 1, updatedProduct);
+  newState = {
+    products: splicedArray,
+    total: state.total + action.product.price,
+  };
+  return newState;
+};
+
+const removesQuantityFromProductInObject = (state, productIndex) => {
+  const updatedProduct = {
+    ...state.products[productIndex],
+    quantity: state.products[productIndex].quantity - 1,
+  };
+  let splicedArray = [...state.products];
+  splicedArray.splice(productIndex, 1, updatedProduct);
+  return {
+    products: splicedArray,
+    total: state.total - updatedProduct.price,
+  };
+};
+
 const cartReducer = (state = initialState, action) => {
   let newState;
   let productIndex;
-  let splicedArray;
   switch (action.type) {
     case "ADD_PRODUCT_TO_CART":
       productIndex = getProductIndex(state.products, action.product.id);
       if (productIndex === -1) {
-        newState = {
-          products: [...state.products, action.product],
-          total: state.total + action.product.price,
-        };
+        newState = addProductToObject(state, action);
       } else {
-        newState = { ...state };
-        const updatedProduct = {
-          ...state.products[productIndex],
-          quantity: state.products[productIndex].quantity + 1,
-        };
-        splicedArray = [...newState.products];
-        splicedArray.splice(productIndex, 1, updatedProduct);
-        newState = {
-          products: splicedArray,
-          total: state.total + action.product.price,
-        };
+        newState = addQuantityToProductInObject(state, action, productIndex);
       }
       return newState;
     case "REMOVE_PRODUCT_FROM_CART":
       productIndex = getProductIndex(state.products, action.productId);
       if (state.products[productIndex].quantity === 1) {
-        splicedArray = [...state.products];
-        splicedArray.splice(productIndex, 1);
-        newState = {
-          products: splicedArray,
-          total: state.total - state.products[productIndex].price,
-        };
+        newState = removesProductFromObject(state, productIndex);
       } else {
-        const updatedProduct = {
-          ...state.products[productIndex],
-          quantity: state.products[productIndex].quantity - 1,
-        };
-        splicedArray = [...state.products];
-        splicedArray.splice(productIndex, 1, updatedProduct);
-        newState = {
-          products: splicedArray,
-          total: state.total - updatedProduct.price,
-        };
+        newState = removesQuantityFromProductInObject(state, productIndex);
       }
       return newState;
     default:
